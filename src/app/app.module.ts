20ffeafa3700349d67d8a0cb81182b81340b4b6a
Lorenz <lorenzcleymans@hotmail.com>
2018-11-19 11:27:09 +0100

    Bootstrap en navbar toegevoegd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgBootstrapModule} from './sharedModules/ng-bootstrap.module';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgBootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
