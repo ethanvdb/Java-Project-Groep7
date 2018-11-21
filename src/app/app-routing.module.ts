c5e067cb1eb9962361bcf91d81e8c1bf7b708863
dieterverboven <dieter.verboven@outlook.com>
2018-11-21 20:58:36 +0100

    home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { OpdrachtComponent} from './opdracht/opdracht.component';
 import {HistoriekComponent} from './historiek/historiek.component';
 import { BeloningComponent} from './beloning/beloning.component';
 import {LoginComponent} from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path: 'opdracht', component: OpdrachtComponent},
   {path: 'historiek', component: HistoriekComponent},
   {path: 'beloning', component: BeloningComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'home', component: HomeComponent},
+
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
 ];
 
 @NgModule({
